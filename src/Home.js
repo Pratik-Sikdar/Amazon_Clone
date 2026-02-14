a31e685cb938b94f0b81b86f7877805595426f26
Pratik Sikdar <pratiksikdar.8@gmail.com>
2026-02-14 16:53:19 +0530

    Home tab started

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <div>
+            <div className="home">
+                <div className="home_container">
+                    <img src="" alt="home image" className="home_image" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home
